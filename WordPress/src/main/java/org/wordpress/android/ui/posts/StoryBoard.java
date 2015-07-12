e5668eb293b0d43a774894742fa8fda86a73e72a
Nick Hargreaves <hargreaves1234@gmail.com>
2015-07-12 09:59:24 +0300

    set display summary from edit text in dialog

--- WordPress/src/main/java/org/wordpress/android/ui/posts/StoryBoard.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/StoryBoard.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,6 +15,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.wordpress.android.R;
+
+import info.hoang8f.widget.FButton;
 
 /**
  * Created by john on 7/12/2015.
@@ -23,6 +27,8 @@ public class StoryBoard extends ActionBarActivity {
 
     private TextView displaySummary;
     private EditText editTextSummary;
+
+    private String summary="";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,15 +55,57 @@ public class StoryBoard extends ActionBarActivity {
     }
 
     public void showCreateSummaryDialog(){
-        
+
         final Dialog dialog = new Dialog(StoryBoard.this);
-        dialog.setContentView(R.layout.summary_fragment);
         dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.summary_fragment);
         dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        final FButton submitButton = (FButton)dialog.findViewById(R.id.submitButton);
+        submitButton.setEnabled(false);
+
+        final EditText editTextSummary = (EditText)dialog.findViewById(R.id.editTextSummary);
+
+        //find current value of summary
+        summary = "" + displaySummary.getText().toString();
+        //if it's not default & not empty edit editTextSummary
+        if(!summary.equals(getResources().getString(R.string.summary_prompt)) && (!summary.equals(""))){
+            editTextSummary.setText(summary);
+            submitButton.setEnabled(true);
+        }else{
+            summary = "";
+        }
+
+        editTextSummary.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                String newSummary = "" + editTextSummary.getText().toString();
+                if(newSummary.length()>0){
+                    summary = newSummary;
+                    submitButton.setEnabled(true);
+                }else{
+                    summary = "";
+                    submitButton.setEnabled(false);
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
 
         dialog.findViewById(R.id.closeDialog).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (summary.trim().length() > 0) {
+                    displaySummary.setText(summary);
+                } else {
+                    displaySummary.setText(getResources().getString(R.string.summary_prompt));
+                }
                 dialog.dismiss();
             }
         });
