5f13f3c41c918bc0f946ba3388d1fc1cb80f9294
Nick Hargreaves <hargreaves1234@gmail.com>
2015-07-15 15:31:28 +0300

    new_account_user_fragment for reg activity

---
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/EditActivity.java
@@ -0,0 +1,17 @@
+package org.wordpress.android.ui.accounts;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.wordpress.android.R;
+
+/**
+ * Created by john on 7/15/2015.
+ */
+public class EditActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_account_activity);
+    }
+}
