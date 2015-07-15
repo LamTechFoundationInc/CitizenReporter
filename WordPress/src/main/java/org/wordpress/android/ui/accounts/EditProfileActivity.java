c9882b2ef9270081a85db53ab50b8ed21c5ed315
Nick Hargreaves <hargreaves1234@gmail.com>
2015-07-15 19:30:10 +0300

    even editprofile needs an up button

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/EditProfileActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/EditProfileActivity.java
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.accounts;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import org.wordpress.android.R;
 
@@ -20,5 +21,13 @@ public class EditProfileActivity extends ActionBarActivity {
         getSupportActionBar().setTitle(getResources().getString(R.string.my_ripoti));
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
